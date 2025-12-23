07eaa952520dd3d974e1953d61b6013ff5ccb9ee
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2025-12-23 10:59:34 +0530

    updated google tag

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,22 @@
-import "./globals.css";
-import LayoutWrapper from "./LayoutWrapper";
+import './globals.css';
+import LayoutWrapper from './LayoutWrapper';
 
 export const metadata = {
-  title: "Nivya Automobiles",
+  title: 'Nivya Automobiles',
   description:
-    "Explore new and used Maruti Suzuki cars in Hyderabad with Nivya Automobiles. Find the best deals, offers, and services.",
+    'Explore new and used Maruti Suzuki cars in Hyderabad with Nivya Automobiles. Find the best deals, offers, and services.',
+  verification: {
+    google: 'SqUUPnh17HPH2EXh3vAwJlyUgO3Hak6wy_ywVW6CzDw',
+  },
   icons: {
-    icon: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
   },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <LayoutWrapper>{children}</LayoutWrapper>
     </html>
   );
