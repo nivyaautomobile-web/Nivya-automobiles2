98d62236f6025e784e7bbb5b048e953b596258e1
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2025-12-23 11:18:58 +0530

    updated google tag

--- app/layout.js
+++ app/layout.js
@@ -33,8 +33,31 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-LP6Q5Z2HK2');
           `}
         </Script>
+        {/* Google Tag Manager */}
+        <Script id='gtm-script' strategy='afterInteractive'>
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M6KJ9FF2');
+          `}
+        </Script>
       </head>
-      <LayoutWrapper>{children}</LayoutWrapper>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M6KJ9FF2"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <LayoutWrapper>{children}</LayoutWrapper>
+      </body>
     </html>
   );
 }
