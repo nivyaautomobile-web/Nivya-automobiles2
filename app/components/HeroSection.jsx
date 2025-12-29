e47683c78377d52409e4543e293f2de6b41f1ee6
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2025-12-29 11:39:00 +0530

    updated banner 2

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -4,14 +4,14 @@ import Banner from './Banner';
 
 export default function HeroSection() {
   const sliders = [
-    {
-      desktopImg:
-        '/images/Nivya-Web-Banner-Year-End.webp',
-      mobileImg:
-        'https://www.skyautomobiles.in/_next/image?url=%2Fimages%2Fother%2Fmain_swift_mobile.webp&w=1080&q=75',
-      alt: 'Monsoon',
-      link: '/vehicles/swift',
-    },
+    // {
+    //   desktopImg:
+    //     '/images/Nivya-Web-Banner-Year-End.webp',
+    //   mobileImg:
+    //     'https://www.skyautomobiles.in/_next/image?url=%2Fimages%2Fother%2Fmain_swift_mobile.webp&w=1080&q=75',
+    //   alt: 'Monsoon',
+    //   link: '/vehicles/swift',
+    // },
     {
       desktopImg:
         '/images/Nivya-Web-Banner-Accessories.webp',
