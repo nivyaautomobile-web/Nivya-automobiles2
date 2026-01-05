7ca7b9980c7bd8a79d8e58a8ecb71f58757f7ac8
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2026-01-05 11:42:15 +0530

    updated about content

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+
+import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
@@ -41,16 +43,12 @@ export default function About() {
                 </h2>
 
                 <p className="mb-8 leading-relaxed text-gray-600">
-                  Nivya Automobiles has been at the forefront of premium and
-                  luxury automotive retail in India. With 139 state-of-the-art
-                  facilities across 32 cities in 12 states and union territories,
-                  our journey is a testament to our unwavering commitment to
-                  delivering unparalleled automotive experiences.
+                  Built on the values of trust, transparency and values of our leaders, Mr Mallu Reddy Niveditha and Mr Mallu Venkata Siva Prasad Reddy, Nivya Automobiles promises a seamless car-purchasing journey. Through this Arena showroom located at NH-44, Rajeev Colony, Near Shilparamam, Anantapur, Andhra Pradesh, we offer modern facilities, end-to-end assistance and the spirit for improvement. At Nivya Automobiles, we aim to redefine what customers expect and deliver more than that. Visit us today!
                 </p>
 
-                <button className="bg-[#283791]  text-white px-8 py-4 font-medium rounded-sm hover:bg-red-700 transition">
+                <Link href={"/about"}><button className="bg-[#283791]  text-white px-8 py-4 font-medium rounded-sm hover:bg-red-700 transition">
                   More About Us
-                </button>
+                </button></Link>
               </>
             )}
           </div>
@@ -61,8 +59,8 @@ export default function About() {
               <Skeleton height={350} width={"100%"} className="rounded-lg" />
             ) : (
               <div className="w-full overflow-hidden rounded-lg shadow-md">
-                <img src="/images/showroom.jpeg"/>
-          
+                <img src="/images/showroom.jpeg" />
+
               </div>
             )}
           </div>
