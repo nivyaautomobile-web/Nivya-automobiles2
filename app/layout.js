9fa2902647595e07efe8969a16543c56a32c9b24
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2026-02-06 11:55:44 +0530

    updated

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,7 @@ import Script from 'next/script';
 import './globals.css';
 import LayoutWrapper from './LayoutWrapper';
 import ModalConf from './components/ModalConf';
-import CountdownTimer from './components/forms/test.jsx/CountdownTimer';
-import NewYearWrapper from './components/forms/test.jsx/NewYearWrapper';
+
 import NumberPopup from './components/forms/Popup';
 
 export const metadata = {
@@ -22,50 +21,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <head>
-        {/* Google Tag Manager */}
-        <Script id='gtm-script' strategy='afterInteractive'>
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M6KJ9FF2');
-          `}
-        </Script>
-
-        <Script
-          src='https://www.googletagmanager.com/gtag/js?id=G-LP6Q5Z2HK2'
-          strategy='afterInteractive'
-        />
-        <script id='ga4-init' strategy='afterInteractive'>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '');
-          `}
-        </script>
-         
-      </head>
-
+      <head></head>
       <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src='https://www.googletagmanager.com/ns.html?id=GTM-M6KJ9FF2'
-            height='0'
-            width='0'
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-
         <LayoutWrapper>
           <ModalConf />
-          <CountdownTimer target='2025-12-31T23:59:00' />
-          {/* <NewYearWrapper />  */}
           <NumberPopup />
           {children}
         </LayoutWrapper>
